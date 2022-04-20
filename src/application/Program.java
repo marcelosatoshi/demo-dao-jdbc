a7fcc1c71f769f7daba2676909dfb2c8e0cccc30
Marcelo Gomes <marcelogomestsigti@gmail.com>
2022-04-20 19:59:34 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -30,6 +31,12 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println();
+		
+		Seller newSeller = new Seller(null,"Greg", "greg@gmail.com" , new Date(), 4000.0 , new Department(2,null));
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! new id = " + newSeller.getId());
 	}
 
 }
