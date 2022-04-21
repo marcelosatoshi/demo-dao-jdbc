c4c2c893196dd577cf56f850c88ebfd65e2f870f
Marcelo Gomes <marcelogomestsigti@gmail.com>
2022-04-20 21:37:33 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,6 +37,16 @@ public class Program {
 		Seller newSeller = new Seller(null,"Greg", "greg@gmail.com" , new Date(), 4000.0 , new Department(2,null));
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! new id = " + newSeller.getId());
+		
+		
+		System.out.println();
+		
+		seller = sellerDao.findById(1);
+		seller.setName("Corolla");
+		seller.setBaseSalary(9000.0);
+		sellerDao.update(seller);
+		System.out.println("Update complete!");
+		
 	}
 
 }
